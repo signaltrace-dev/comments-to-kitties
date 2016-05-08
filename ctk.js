fa25ad65a720e72742581a4c9d1026103f2c7554
Jeff Glenn <jeff@signaltrace.net>
2016-05-08 14:25:05 -0400

    Use polling to check for comment sections (handle 'Load Comments' buttons, etc)

--- ctk.js
+++ ctk.js
@@ -1,29 +1,45 @@
 $(document).ready(function(){
-  var urls = [];
-  $.get("http://thecatapi.com/api/images/get?format=xml&size=small&results_per_page=10&api_key=ODM5Mjg", function(data){
-    var images = $(data).find("image");
-    $(images).each(function(){
-      var image = this;
-      $(image).find('url').each(function(){
-        var url = this.textContent;
-        urls.push(url);
-      });
-      $('#disqus_thread, .fb-comments').each(function(){
-        var elem = this;
-        var content = '';
 
-        var count = images.length;
-        $(urls).each(function(index, value){
-          var url = value;
-          content += "<img src='" + url + "'/>";
-          if(!--count){
-            $(elem).replaceWith(content);
-          }
+  var hideComments = function(){
+    var urls = [];
+
+    $.get("http://thecatapi.com/api/images/get?format=xml&size=small&results_per_page=10&api_key=ODM5Mjg", function(data){
+      var images = $(data).find("image");
+      $(images).each(function(){
+        var image = this;
+        $(image).find('url').each(function(){
+          var url = this.textContent;
+          urls.push(url);
+        });
+        $('#disqus_thread, .fb-comments').each(function(){
+          var elem = this;
+          var content = '<div id="comment-kitties">';
+
+          var count = images.length;
+          $(urls).each(function(index, value){
+            var url = value;
+            content += "<a href='" + url + "'><img src='" + url + "'/></a>";
+            if(!--count){
+              content += '</div>';
+              $(elem).replaceWith(content);
+            }
+          });
         });
       });
-
-      //urls.push(url[0].textContent);
     });
-  });
+  };
+
+  var prevStyle,
+  count = 1,
+  poll = window.setInterval(function(){
+    var commentsElem = $('#disqus_thread, .fb-comments');
+
+    if(commentsElem.length > 0){
+      window.clearInterval(poll);
+      hideComments();
+    }
+  }, 1000);
+
+
 
 });

c26bf7ea720b4166143dee9535f28313812d1489
Jeff Glenn <jeff@signaltrace.net>
2016-05-08 14:23:56 -0400

    Change from background script to content_scripts

--- background.js
+++
@@ -1,7 +0,0 @@
-
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(null, { file: "jquery-2.2.3.min.js" }, function() {
-      chrome.tabs.executeScript(null, { file: "ctk.js" });
-  });
-});
